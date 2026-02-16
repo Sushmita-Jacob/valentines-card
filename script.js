4c30927f5912b71bb3513b20afff31eed12d129f
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-02-16 01:18:32 +0000

    makes an envelope open on click

---
+++ script.js
@@ -0,0 +1,8 @@
+const card = document.getElementById('card')
+const envelope = document.getElementById('envelope-img') 
+const message = document.getElementById('message')
+
+function switchImage() {
+    console.log("testing")
+    envelope.src="open.jpg";
+}
