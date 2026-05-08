ed6a2cdc00e35141bef801b186dac7daf6c34fdc
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-05-08 23:37:12 +0000

    added counter when closing envelops to create a clicker game (inspired by universal paperclips)

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const card = document.getElementById('card')
 const envelope = document.getElementById('envelope-img') 
 const message = document.getElementById('message')
+var count=0;
 
 function switchImage() {
     console.log("testing")
+    document.getElementById("counter").innerHTML = count;
     if (envelope.src.endsWith("open.jpg")) {
         envelope.src="closed.jpg";
     }
     else if (envelope.src.endsWith("closed.jpg")) {
         envelope.src="open.jpg";
+        count++;
     }
     confetti();
 }
