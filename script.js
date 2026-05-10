9fba03c91d81cbb154f84acbba28b6b1c5474c61
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-05-10 17:22:49 +0000

    added shop items and increments and fixed image backgrounds

--- script.js
+++ script.js
@@ -1,26 +1,84 @@
 const card = document.getElementById('card')
 const envelope = document.getElementById('envelope-img') 
 const message = document.getElementById('message')
-var count=0;
+var count=1;
 const shop1 = document.getElementById('shop1id')
+const shop1bought = document.getElementById('shop1bought')
+const shop2 = document.getElementById('shop2id')
+const shop2bought = document.getElementById('shop2bought')
+const shop3 = document.getElementById('shop3id');
+const shop3bought = document.getElementById('shop3bought');
+const openClose = document.getElementById("open-close");
+var counter = document.getElementById('counter');
+let autoEnv = false
+let mailman = false
+let pigeon = false
 
 function switchImage() {
     console.log("testing")
     document.getElementById("counter").innerHTML = count;
-    if (envelope.src.endsWith("open.jpg")) {
-        envelope.src="closed.jpg";
-        confetti();
+    if (autoEnv == false) {
+        if (envelope.src.endsWith("open.png")) {
+            envelope.src="closed.png";
+            openClose.textContent = "Click to open the envelope and put in a card!";
+            confetti();
+        }
+        else if (envelope.src.endsWith("closed.png")) {
+            envelope.src="open.png";
+            openClose.textContent = "Click to seal, send, and get a new envelope!";
+            count++;
+        }
     }
-    else if (envelope.src.endsWith("closed.jpg")) {
-        envelope.src="open.jpg";
+    if (autoEnv == true) {
+        envelope.src="closed.png";
+        openClose.textContent = "Click to send the envelope!";
         count++;
+        confetti();
     }
 }
 
 shop1.addEventListener('click', function() {
-    if (count >= 15) {
-        count = count - 15
+    if (count >= 30) {
+        count = count - 30;
+        confetti();
+        shop1bought.textContent = "(Bought!)"
         shop1.remove();
+        counter.textContent = count;
+        autoEnv = true;
     }
-    
+    }
+)
+
+shop2.addEventListener('click', function(){
+    if (count>=50) {
+        count = count - 50;
+        confetti();
+        shop2bought.textContent = "(Bought!)"
+        shop2.remove();
+        counter.textContent = count;
+        mailman = true;
+        if (mailman == true) {
+            setInterval(() => {
+                count = count + 1;
+                counter.textContent = count;
+            }, 1000);
+        }
+    }
+})
+
+shop3.addEventListener('click', function(){
+    if (count>=100) {
+        count = count - 100;
+        confetti();
+        shop3bought.textContent = "(Bought!)";
+        shop3.remove();
+        counter.textContent = count;
+        pigeon=true;
+        if (pigeon == true) {
+            setInterval(() => {
+                count = count + 3;
+                counter.textContent = count;
+            }, 1000);
+        }
+    }
 })
